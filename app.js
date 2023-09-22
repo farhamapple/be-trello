ca45536f1fa7b02a320be703b262fe06733bfd1d
White.wolf <white.wolf.90@gmail.com>
2023-09-22 23:18:40 +0700

    Initialize

---
+++ app.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const todosRouter = require('./app/api/todos/router');
+const URL = '/api/v1'
+const app = express();
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+    res.json({
+      message : "Welcome API Clone Trello",
+      version : "1.0"
+    })
+})
+
+app.use(`${URL}`, todosRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ app/api/todos/router.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/todos', function(req, res, next) {
+  res.json({
+    message : "Welcome Todos Page"
+  })
+});
+
+module.exports = router;
