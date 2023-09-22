13d0f8829c82aead4ec3be7c613dfa9bd01535e4
White.wolf <white.wolf.90@gmail.com>
2023-09-23 00:40:09 +0700

    feat: Update, Destroy Done

--- app/api/todos/controller.js
+++ app/api/todos/controller.js
@@ -79,15 +79,28 @@ module.exports = {
     destroy : async(req, res) =>{
         const { id } = req.params;
         
-        Todo.destroy({
-             where: {id: id}
-        }).then((todo) => {
-            res.status(200).json({
-                message : 'success',
-                data : todo
-            }) 
-        }).catch((err) => {
-         console.log(err)
-        })
+        // Todo.destroy({
+        //      where: {id: id}
+        // }).then((todo) => {
+        //     res.status(200).json({
+        //         message : 'success',
+        //         data : todo
+        //     }) 
+        // }).catch((err) => {
+        //  console.log(err)
+        // })
+
+        Todo.findOne({
+            where: {id: id}
+       }).then((todo) => {
+                todo.destroy().then(() => {
+                    res.status(200).json({
+                        message : 'success',
+                        data : todo
+                    })
+                })
+       }).catch((err) => {
+        console.log(err)
+       })
      },
 }
