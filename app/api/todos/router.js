1da877c44479c36cc564ffd505201599331249ef
White.wolf <white.wolf.90@gmail.com>
2023-09-24 15:28:57 +0700

    feat: Validation Todo

--- app/api/todos/router.js
+++ app/api/todos/router.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const { getAll, create, getById, update, destroy } = require('./controller')
-
+const { validateCreate, validateOne, validateCreateUpdate } = require('./validation')
 
 router.get('/todos', getAll);
-router.post('/todos', create);
-router.get('/todos/:id', getById);
-router.put('/todos/:id', update);
-router.delete('/todos/:id', destroy);
+router.post('/todos', validateCreate, create);
+router.get('/todos/:id', validateOne, getById);
+router.put('/todos/:id', validateCreateUpdate, update);
+router.delete('/todos/:id', validateOne, destroy);
 module.exports = router;
---
+++ app/api/todos/validation.js
@@ -0,0 +1,81 @@
+const { body, param, validationResult } = require('express-validator');
+const { Todo } = require('../../db/models');
+module.exports = {
+
+    validateCreate: [
+        body('name').notEmpty().withMessage('name is required'),
+        (req, res, next) =>  {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(422).json({
+                    message : "error",
+                    error : errors.array(),
+                })
+            }
+
+            next()
+        },
+    ],
+
+    validateOne : [
+        param('id').notEmpty().withMessage('Param ID is Required')
+        .bail()
+        .isNumeric()
+        .withMessage('id must be an integer')
+        .bail()
+        .custom(async(value, {req}) => {
+            const checking = await Todo.findOne({
+                where: {id : value}
+            })
+
+            if(checking === null){
+                return Promise.reject();
+            }
+        })
+        .withMessage('param id Not Found'),
+        (req, res, next) =>  {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(422).json({
+                    message : "error",
+                    error : errors.array(),
+                })
+            }
+
+            next()
+        }
+    ],
+
+    validateCreateUpdate : [
+        param('id').notEmpty().withMessage('Param ID is Required')
+        .bail()
+        .isNumeric()
+        .withMessage('id must be an integer')
+        .bail()
+        .custom(async(value, {req}) => {
+            const checking = await Todo.findOne({
+                where: {id : value}
+            })
+
+            if(checking === null){
+                return Promise.reject();
+            }
+        })
+        .withMessage('param id Not Found'),
+        body('name').notEmpty().withMessage('name is required'),
+        (req, res, next) =>  {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return res.status(422).json({
+                    message : "error",
+                    error : errors.array(),
+                })
+            }
+
+            next()
+        }
+    ],
+
+
+
+}
