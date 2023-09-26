3e6b0cbb7aaa5f94e3be497414e8b8f7dfd84bcc
White.wolf <white.wolf.90@gmail.com>
2023-09-26 23:14:48 +0700

    refactor: Json Response in Item Controller

--- app/api/items/controller.js
+++ app/api/items/controller.js
@@ -1,4 +1,5 @@
 const { Todo, Item } = require('../../db/models');
+var response = require('../../utils/response');
 
 module.exports = {
     getById : async(req, res, next) =>{
@@ -8,10 +9,12 @@ module.exports = {
                 where: {id: id}
             })
 
-            res.status(200).json({
-                message : 'success',
-                data : result
-            })
+            // res.status(200).json({
+            //     message : 'success',
+            //     data : result
+            // })
+
+            response.ok(result, 'Get Data Item By ID', '00', 200, false, res)
         }catch(err){
             // console.log(err)
             next(err)
@@ -25,10 +28,12 @@ module.exports = {
                 name, TodoId
             })
 
-            res.status(200).json({
-                message : 'success',
-                data : result
-            })
+            // res.status(200).json({
+            //     message : 'success',
+            //     data : result
+            // })
+
+            response.ok(result, 'Insert Data to Items', '00', 200, false, res)
         }catch(err){
             // console.log(err)
             next(err)
@@ -47,10 +52,12 @@ module.exports = {
                     name : name,
                     TodoId : TodoId
                 }).then(() => {
-                    res.status(200).json({
-                        message : 'success',
-                        data : item
-                    })
+                    // res.status(200).json({
+                    //     message : 'success',
+                    //     data : item
+                    // })
+
+                    response.ok(item, 'Update Data Item By Id, name and TodoId', '00', 200, false, res)
                 })
        }).catch((err) => {
         // console.log(err)
@@ -76,10 +83,12 @@ module.exports = {
             where: {id: id}
         }).then((item) => {
                 item.destroy().then(() => {
-                    res.status(200).json({
-                        message : 'success',
-                        data : item
-                    })
+                    // res.status(200).json({
+                    //     message : 'success',
+                    //     data : item
+                    // })
+
+                    response.ok(item, 'Delete Data Item By Id', '00', 200, false, res)
                 })
         }).catch((err) => {
             next(err)
@@ -98,10 +107,12 @@ module.exports = {
 
             await result.save();
 
-            res.status(200).json({
-                message: 'success',
-                data : result
-            })
+            // res.status(200).json({
+            //     message: 'success',
+            //     data : result
+            // })
+            response.ok(result, 'Move Data Item by Id and TargetTodoId', '00', 200, false, res)
+            
 
             // Item.findOne({
             //     where: {id : id}
